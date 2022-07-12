011e671e03493b68e30cbb638058cdeb084f9b53
Nick081 <105968998+w0oo@users.noreply.github.com>
2022-07-12 12:47:45 +0800

    Create walinepl.js

---
+++ walinepl.js
@@ -0,0 +1,8 @@
+Waline.init({
+      el: '#waline',
+      serverURL: 'https://minefile-zyfxz.vercel.app',
+      
+      emoji: [
+      'https://unpkg.com/@waline/emojis@1.0.1/bilibili',
+      ],
+});
