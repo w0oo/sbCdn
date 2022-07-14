119f621a60d727200ad642fe4dc0807c4f00feb7
Nick081 <105968998+w0oo@users.noreply.github.com>
2022-07-14 22:01:33 +0800

    Update walinepl.js

--- walinepl.js
+++ walinepl.js
@@ -5,4 +5,23 @@ Waline.init({
       emoji: [
       'https://unpkg.com/@waline/emojis@1.0.1/bilibili',
       ],
+        
+      imageUploader: function (file) {
+          let formData = new FormData();
+          let headers = new Headers();
+
+          formData.append('file', file);
+          headers.append('Authorization', '11bab3a0fddbc07c302a1a6cdd595e32');
+          headers.append('Accept', 'application/json');
+
+          return fetch('https://imgtp.com', {
+            method: 'POST',
+            headers: headers,
+            body: formData,
+          })
+            .then((resp) => resp.json())
+            .then((resp) => resp.data.links.url);
+        },
+      });
+
 });
