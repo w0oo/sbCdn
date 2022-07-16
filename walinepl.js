70ec743fa1188396c89db0041488e66ce10b4ecf
Nick081 <105968998+w0oo@users.noreply.github.com>
2022-07-16 09:28:05 +0800

    Update walinepl.js

--- walinepl.js
+++ walinepl.js
@@ -8,6 +8,20 @@ Waline.init({
 
       login: 'force',
      
-      imageUploader: 'false',
+      imageUploader: function (file) {
+          let formData = new FormData();
+          let headers = new Headers();
 
-});
+          formData.append('file', file);
+          headers.append('Authorization', '11bab3a0fddbc07c302a1a6cdd595e32');
+          headers.append('Accept', 'application/json');
+
+          return fetch('https://imgtp.com/api/upload', {
+            method: 'POST',
+            headers: headers,
+            body: formData,
+          })
+            .then((resp) => resp.json())
+            .then((resp) => resp.data.links.url);
+        },
+      });
