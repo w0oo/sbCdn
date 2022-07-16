2fa78018a8b9723d03b354102cd85711d13a82ec
Nick081 <105968998+w0oo@users.noreply.github.com>
2022-07-16 15:54:44 +0800

    Update walinepl.js

--- walinepl.js
+++ walinepl.js
@@ -1,23 +1,21 @@
 const waline = Waline.init({
         el: '#waline',
         serverURL: 'https://minefile-zyfxz.vercel.app',
+        dark: 'auto',
         lang: 'zh-CN',
         login: 'force',
         emoji: ['https://unpkg.com/@waline/emojis@1.0.1/bilibili',],
-        imageUploader: function (file) {
-          let formData = new FormData();
-          let headers = new Headers();
-
+        uploadImage: function(file) {
+          const formData = new FormData();
           formData.append('file', file);
-          headers.append('Authorization', '188|JhcKLTntWgVIo4s7HlElnhvNS0OpEqFdjS4uRmQk');
-          headers.append('Accept', 'application/json');
-
-          return fetch('https://7bu.top/api/v1/upload', {
-            method: 'POST',
-            headers: headers,
-            body: formData,
-          })
-            .then((resp) => resp.json())
-            .then((resp) => resp.data.links.url);
+          // formData.append('strategy_id', 0); // 可选策略ID
+         return fetch('https://7bu.top/api/v1/upload', {
+        headers: {
+          'Authorization': 'Bearer 188|JhcKLTntWgVIo4s7HlElnhvNS0OpEqFdjS4uRmQk'
         },
-      });
+        method: 'POST',
+      body: formData
+      }).then(resp => resp.json()).then(resp => resp.data.links.url);
+    },
+    copyright: false,
+  });
