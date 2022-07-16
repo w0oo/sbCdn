03add3da743d7ae231d6c79a189a41c9b3715d03
Nick081 <105968998+w0oo@users.noreply.github.com>
2022-07-16 16:05:47 +0800

    Update walinepl.js

--- walinepl.js
+++ walinepl.js
@@ -1,15 +1,17 @@
-const waline = Waline.init({
-        el: '#waline',
-        serverURL: 'https://minefile-zyfxz.vercel.app',
-        dark: 'auto',
-        lang: 'zh-CN',
-        login: 'force',
-        emoji: ['https://unpkg.com/@waline/emojis@1.0.1/bilibili',],
-        uploadImage: function(file) {
-          const formData = new FormData();
-          formData.append('file', file);
-          // formData.append('strategy_id', 0); // 可选策略ID
-         return fetch('https://7bu.top/api/v1/upload', {
+<div id="waline"></div>
+<script>
+  Waline({
+    el: '#waline',
+    serverURL: 'https://minefile-zyfxz.vercel.app',
+    emoji: ['https://unpkg.com/@waline/emojis@1.0.1/bilibili'],
+    dark: 'auto',
+    lang: 'zh-CN',
+    login: 'force',
+    uploadImage: function(file) {
+      const formData = new FormData();
+      formData.append('file', file);
+      // formData.append('strategy_id', 0); // 可选策略ID
+      return fetch('https://7bu.top/api/v1/upload', {
         headers: {
           'Authorization': 'Bearer 188|JhcKLTntWgVIo4s7HlElnhvNS0OpEqFdjS4uRmQk'
         },
@@ -19,3 +21,4 @@ const waline = Waline.init({
     },
     copyright: false,
   });
+</script>
