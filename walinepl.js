55ea0dfd76e78dfe719aac3e420cc192dadf2459
Nick081 <105968998+w0oo@users.noreply.github.com>
2022-07-17 09:38:11 +0800

    Update walinepl.js

--- walinepl.js
+++ walinepl.js
@@ -8,7 +8,7 @@ const waline = Waline.init({
     imageUploader : function(file) {
       const formData = new FormData();
       formData.append('file', file);
-      formData.append('strategy_id', 22);
+      formData.append('strategy_id', 5);
       return fetch('https://imgs.top/api/v1/upload', {
         headers: {
           'Authorization': 'Bearer 312|nz2LM3xt5fMyw6EwGITRGIm655ZPRZSQrVlGIB8e'
